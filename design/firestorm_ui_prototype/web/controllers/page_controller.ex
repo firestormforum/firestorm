5fba5a58fe1094b3c865119ae1deb6236d0ba664
Josh Adams <josh.rubyist@gmail.com>
2017-02-15 10:21:51 -0600

    Some more prototyping with slightly more realistic data

--- design/firestorm_ui_prototype/web/controllers/page_controller.ex
+++ design/firestorm_ui_prototype/web/controllers/page_controller.ex
@@ -7,19 +7,22 @@ defmodule FirestormUiPrototype.PageController do
   end
 
   def home(conn, _params) do
-    { elixir, otp } = mock_categories()
+    { elixir, otp, elm, elm_mdl } = mock_categories()
     threads = %{
-      otp: mock_threads()
+      otp: mock_otp_threads(),
+      elm_mdl: mock_elm_mdl_threads()
     }
 
-    render conn, "home.html", elixir: elixir, otp: otp, threads: threads
+    render conn, "home.html", elixir: elixir, otp: otp, threads: threads, elm: elm, elm_mdl: elm_mdl
   end
 
   def mock_categories() do
     elixir = %Category{title: "Elixir"}
     otp = %Category{title: "OTP"}
+    elm = %Category{title: "Elm"}
+    elm_mdl = %Category{title: "elm-mdl"}
 
-    { elixir, otp }
+    { elixir, otp, elm, elm_mdl }
   end
 
   def mock_users() do
@@ -30,7 +33,7 @@ defmodule FirestormUiPrototype.PageController do
     { adamdill, knewter, franzejr}
   end
 
-  def mock_threads() do
+  def mock_otp_threads() do
     { adamdill, knewter, franzejr } = mock_users()
 
     how_do_i_otp = %Thread{posts: [mock_post(), mock_post()], title: "How do I OTP?"}
@@ -41,6 +44,18 @@ defmodule FirestormUiPrototype.PageController do
       { how_do_i_otp, adamdill },
       { do_you_even_process, knewter },
       { dhtml, franzejr }
+    ]
+  end
+
+  def mock_elm_mdl_threads() do
+    { adamdill, knewter, _franzejr } = mock_users()
+
+    elm_mdl_textfields = %Thread{posts: [mock_post(), mock_post()], title: "elm_mdl textfields"}
+    extracting = %Thread{posts: [mock_post(), mock_post()], title: "Extracting Modules, Nested Records, and Parameterized Types"}
+
+    [
+      { elm_mdl_textfields, knewter },
+      { extracting, adamdill },
     ]
   end
 
