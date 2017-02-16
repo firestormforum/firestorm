3d2c8aeb9b7e7191737a3b33bf66bc43fd5631ed
Josh Adams <josh.rubyist@gmail.com>
2017-02-16 00:52:39 -0600

    Get mix test working from root

--- firestorm/apps/firestorm_data/mix.exs
+++ firestorm/apps/firestorm_data/mix.exs
@@ -12,7 +12,6 @@ defmodule FirestormData.Mixfile do
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      aliases: aliases(),
       deps: deps()
     ]
   end
@@ -32,12 +31,6 @@ defmodule FirestormData.Mixfile do
       {:postgrex, ">= 0.0.0"},
       {:arbor, "~> 1.0.3"},
       {:ecto_autoslug_field, "~> 0.2"},
-    ]
-  end
-
-  defp aliases do
-    [
-      "test": ["ecto.drop --quiet", "ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 end
