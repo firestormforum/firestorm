e356c607c58f885fb92a62954404a1f345895e46
Josh Adams <josh.rubyist@gmail.com>
2017-02-23 12:34:13 -0600

    Implebit of new post

--- apps/firestorm_web/web/views/view_helpers.ex
+++ apps/firestorm_web/web/views/view_helpers.ex
@@ -1,9 +1,15 @@
 defmodule FirestormWeb.ViewHelpers do
+  import Phoenix.HTML.Tag
+
   def image_path(path) do
     if Mix.env == :prod do
       "/images/#{path}"
     else
       "http://localhost:8080/assets/images/#{path}"
     end
+  end
+
+  def icon(name) do
+    tag(:i, class: "fa fa-#{name}")
   end
 end
