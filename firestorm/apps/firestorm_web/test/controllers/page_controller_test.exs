753afa1aeb159bf26c252de557d479777a498e5c
Josh Adams <josh.rubyist@gmail.com>
2017-02-23 06:05:48 -0600

    Fix dumb wrong test

--- firestorm/apps/firestorm_web/test/controllers/page_controller_test.exs
+++ firestorm/apps/firestorm_web/test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule FirestormWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Firestorm"
   end
 end
