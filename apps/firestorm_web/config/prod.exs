d9bbe0a3552fd3c89800b8b87f434023041eac87
Josh Adams <josh.rubyist@gmail.com>
2017-02-23 09:43:42 -0600

    Fix a bug in heroku prod config re: host
    didn't delete part oops

--- apps/firestorm_web/config/prod.exs
+++ apps/firestorm_web/config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :firestorm_web, FirestormWeb.Endpoint,
   http: [:inet6, port: {:system, "PORT"}],
-  url: [scheme: "https", host: "quiet-tor-85807mysterious-meadow-6277.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "quiet-tor-85807.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
