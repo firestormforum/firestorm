3da6a5fa0079fd9a2b65c22550f7eebac4820f8a
Josh Adams <josh.rubyist@gmail.com>
2017-02-23 06:42:05 -0600

    Set up master for production heroku deployment

---
+++ apps/firestorm_data/config/prod.exs
@@ -0,0 +1,7 @@
+use Mix.Config
+
+config :firestorm_data, FirestormData.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
--- apps/firestorm_web/config/prod.exs
+++ apps/firestorm_web/config/prod.exs
@@ -13,8 +13,10 @@ use Mix.Config
 # which you typically run after static files are built.
 config :firestorm_web, FirestormWeb.Endpoint,
   http: [:inet6, port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "quiet-tor-85807mysterious-meadow-6277.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -59,4 +61,3 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
