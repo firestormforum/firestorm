35fcca6902e34ebd104436143d280d14059ed972
Josh Adams <josh.rubyist@gmail.com>
2017-02-24 20:42:03 -0600

    Add nav_items overridable to navigation defaults

--- apps/firestorm_web/lib/firestorm_web/navigation/defaults.ex
+++ apps/firestorm_web/lib/firestorm_web/navigation/defaults.ex
@@ -1,9 +1,32 @@
 defmodule FirestormWeb.Navigation.Defaults do
+  import FirestormWeb.Session
+  import FirestormWeb.Router.Helpers
+
+  defmodule NavItem do
+    defstruct text: "", path: "", options: []
+  end
+
   defmacro __using__(_) do
     quote do
-      def back(_other, _assigns), do: nil
+      def back(_other, conn), do: nil
 
-      defoverridable [back: 2]
+      def nav_items(_other, conn) do
+        defaults =
+          [
+            %NavItem{text: "Home", path: page_path(conn, :home)}
+          ]
+
+        if logged_in?(conn) do
+          defaults ++
+            [
+              %NavItem{text: "Log Out", path: auth_path(conn, :delete), options: [ method: :delete ]}
+            ]
+        else
+          defaults
+        end
+      end
+
+      defoverridable [back: 2, nav_items: 2]
     end
   end
 end
