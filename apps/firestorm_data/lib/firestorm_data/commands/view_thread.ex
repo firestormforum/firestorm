76e37d83c7f0ac7fbfc069e5a36b4e9d2c589f41
Josh Adams <josh.rubyist@gmail.com>
2017-03-03 20:28:29 -0600

    Add command to view a thread

---
+++ apps/firestorm_data/lib/firestorm_data/commands/view_thread.ex
@@ -0,0 +1,63 @@
+defmodule FirestormData.Commands.ViewThread do
+  @moduledoc """
+  A command to view a thread. This records evidence that a given user viewed a given thread at a given time.
+  """
+
+  use FirestormData.Command
+
+  embedded_schema do
+    field :user_id, :integer
+    field :thread_id, :integer
+  end
+
+  @required_fields ~w(user_id thread_id)a
+  @optional_fields ~w()a
+
+  def changeset(record, params \\ %{}) do
+    record
+    |> cast(params, @required_fields ++ @optional_fields)
+    |> validate_required(@required_fields)
+  end
+
+  # Imagine we've extracted this to something like `Firestorm.run`
+  # and it can handle all of our commands.
+  #
+  # For now we'll just put them in each command til I figure it out :)
+  def run(changeset) do
+    case changeset.valid? do
+      true ->
+        %{user_id: user_id, thread_id: thread_id} =
+          changeset
+          |> apply_changes
+
+        thread = Repo.get(Thread, thread_id)
+
+        thread
+        |> Ecto.build_assoc(:views, %{user_id: user_id})
+        |> Repo.insert
+        |> handle_result(changeset)
+
+      false ->
+        {:error, changeset}
+    end
+  end
+
+  def handle_result({:ok, view}, _changeset) do
+    {:ok, view.id}
+  end
+  def handle_result({:error, changes}, changeset) do
+    # need to do better than this
+    {:error, Changeset.add_error(changeset, :user_id, "There was an error", changes.errors)}
+  end
+
+  defp insert_post(%{thread: %{id: thread_id}}, user_id, body) do
+      %Post{}
+      |> Post.changeset(%{user_id: user_id, thread_id: thread_id, body: body})
+      |> Repo.insert
+  end
+
+  defp thread_changeset(user_id, category_id, title) do
+    %Thread{}
+    |> Thread.changeset(%{category_id: category_id, title: title, user_id: user_id})
+  end
+end
---
+++ apps/firestorm_data/test/firestorm_data/commands/view_thread_test.exs
@@ -0,0 +1,60 @@
+defmodule FirestormData.Commands.ViewThreadTest do
+  use ExUnit.Case
+  alias FirestormData.Commands.{CreateCategory, CreateThread, ViewThread}
+  alias FirestormData.{Thread, User, Repo}
+
+  setup do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
+  end
+
+  describe "viewing a thread" do
+    setup [:create_user, :create_category, :create_thread, :view_thread]
+
+    test "returns expected results", %{result: result} do
+      assert {:ok, _some_id} = result
+    end
+
+    test "creates a view in the database", %{thread_id: thread_id} do
+      thread = Repo.get(Thread, thread_id)
+      assert 1 == Thread.view_count(thread)
+    end
+  end
+
+  def create_category(_) do
+    changeset =
+      %CreateCategory{}
+      |> CreateCategory.changeset(%{title: "some title"})
+
+    {:ok, category_id} = CreateCategory.run(changeset)
+
+    {:ok, category_id: category_id}
+  end
+
+  def create_user(_) do
+    changeset =
+      %User{}
+      |> User.changeset(%{username: "sonny"})
+
+    {:ok, user} = Repo.insert(changeset)
+
+    {:ok, user_id: user.id}
+  end
+
+  def create_thread(%{user_id: user_id, category_id: category_id}) do
+    changeset =
+      %CreateThread{}
+      |> CreateThread.changeset(%{user_id: user_id, title: "Some thread", body: "Some body", category_id: category_id})
+
+    {:ok, thread_id} = CreateThread.run(changeset)
+
+    {:ok, thread_id: thread_id}
+  end
+
+  def view_thread(%{user_id: user_id, thread_id: thread_id}) do
+    changeset =
+      %ViewThread{}
+      |> ViewThread.changeset(%{user_id: user_id, thread_id: thread_id})
+
+    {:ok, result: ViewThread.run(changeset) }
+  end
+end
