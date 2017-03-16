fd278258106dfaeb1d82b26164ebcb8007784cd1
Josh Adams <josh.rubyist@gmail.com>
2017-03-15 20:36:52 -0500

    Really basic 'reply to thread via email' feature

--- apps/firestorm_web/lib/firestorm_web/web/controllers/inbound_controller.ex
+++ apps/firestorm_web/lib/firestorm_web/web/controllers/inbound_controller.ex
@@ -1,11 +1,36 @@
 defmodule FirestormWeb.Web.InboundController do
   use FirestormWeb.Web, :controller
   require Logger
+  alias FirestormData.Commands.CreatePost
+
+  @inbound_domain "notifier.firestormforum.org"
+  # Get the thread id out of something like
+  # "thread-123@notifier.firestormforum.org"
+  @thread_email_regex ~r/thread-([0-9]*)@#{@inbound_domain}/
+  # Get the email part out of something like "Josh Adams <josh@dailydrip.com>"
+  @email_regex ~r/[^<]*<?(.*@[^>]*)[^>]*/
 
   def sendgrid(conn, params) do
-    Logger.warn(inspect(params))
+    body = params["text"]
+    from_email_param = params["from"]
+    to_email = params["to"]
 
-    conn
-    |> render("sendgrid.json", %{})
+    with [_,from_email|_] <- Regex.run(@email_regex, from_email_param),
+         [_,thread_id|_] <- Regex.run(@thread_email_regex, to_email),
+         user when not is_nil(user) <-  Repo.get_by(User, email: from_email),
+         {:ok, _} <- create_post(user.id, thread_id, body) do
+      conn
+      |> render("sendgrid.json", %{})
+    else
+      _ ->
+        conn
+        |> render("sendgrid.json", %{})
+    end
+  end
+
+  defp create_post(user_id, thread_id, body) do
+    %CreatePost{}
+    |> CreatePost.changeset(%{user_id: user_id, thread_id: thread_id, body: body})
+    |> CreatePost.run
   end
 end
