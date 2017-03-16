50ddeac478ec4cafd1f1505b09ea35c89c848c9f
Josh Adams <josh.rubyist@gmail.com>
2017-03-16 13:27:13 -0500

    Link to post in notification

--- apps/firestorm_notifier/lib/firestorm_notifier/emails.ex
+++ apps/firestorm_notifier/lib/firestorm_notifier/emails.ex
@@ -26,9 +26,17 @@ defmodule FirestormNotifier.Emails do
     "notifier.firestormforum.org"
   end
 
+  def thread_url(thread) do
+    category_thread_url(Endpoint, :show, category_finder(thread.category), thread.id)
+  end
+
+  def post_url(thread, post) do
+    "#{thread_url(thread)}#post-#{post.id}"
+  end
+
   defp html_body_for(thread, post) do
     """
-    <p>The Firestorm thread #{link(thread.title, to: category_thread_url(Endpoint, :show, category_finder(thread.category), thread.id)) |> safe_to_string} has received a reply:</p>
+    <p>The Firestorm thread #{link(thread.title, to: thread_url(thread)) |> safe_to_string} has received #{link("a reply", to: post_url(thread, post)) |> safe_to_string}:</p>
     <hr />
     #{Markdown.render(post.body)}
     """
@@ -36,7 +44,7 @@ defmodule FirestormNotifier.Emails do
 
   defp text_body_for(thread, post) do
     """
-    The Firestorm thread #{link(thread.title, to: category_thread_url(Endpoint, :show, category_finder(thread.category), thread.id)) |> safe_to_string} has received a reply:\n\n
+    The Firestorm thread #{thread.title} [#{thread_url(thread)}] has received a reply [#{post_url(thread, post)}]:\n\n
     #{post.body}
     """
   end
--- apps/firestorm_notifier/test/firestorm_notifier/emails_test.exs
+++ apps/firestorm_notifier/test/firestorm_notifier/emails_test.exs
@@ -6,7 +6,7 @@ defmodule FirestormNotifier.EmailsTest do
   test "thread new post notifications have appropriate reply-to" do
     user = %{email: "bob@example.com"}
     thread = %{title: "Some thread", id: "123", category: %{id: 1, slug: "foo"}}
-    post = %{body: "Foo"}
+    post = %{body: "Foo", id: 45}
     email = Emails.thread_new_post_notification(user, thread, post)
     assert email.headers["reply-to"] == "reply-thread-123@notifier.firestormforum.org"
   end
