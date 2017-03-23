55e498dfbec4a97ed8d5800a3e6b8b07a9238429
Josh Adams <josh.rubyist@gmail.com>
2017-03-22 19:35:27 -0700

    Don't check origin on ws

--- apps/firestorm_web/lib/firestorm_web/web/channels/user_socket.ex
+++ apps/firestorm_web/lib/firestorm_web/web/channels/user_socket.ex
@@ -10,7 +10,10 @@ defmodule FirestormWeb.Web.UserSocket do
   channel "users:*", FirestormWeb.Web.UsersChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
+  transport :websocket,
+    Phoenix.Transports.WebSocket,
+    timeout: 45_000,
+    check_origin: false
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
