869b4d458e6d3724e5ebd1837bbec1df7e82f8b3
Josh Adams <josh.rubyist@gmail.com>
2017-03-22 19:18:05 -0700

    Fix production host

--- apps/firestorm_web/config/prod.exs
+++ apps/firestorm_web/config/prod.exs
@@ -13,7 +13,11 @@ use Mix.Config
 # which you typically run after static files are built.
 config :firestorm_web, FirestormWeb.Web.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "dogfood.firestormforum.org", port: 443],
+  url: [
+    scheme: "https",
+    host: "firestorm-dogfood.herokuapp.com",
+    port: 443
+  ],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
