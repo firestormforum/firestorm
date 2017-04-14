bcb9d4cdb66f9d000c764dae8bdd9a617dc0e10b
Josh Adams <josh.rubyist@gmail.com>
2017-04-14 00:25:47 -0500

    Add basic umbrella app support and PATRONS file

---
+++ mix.exs
@@ -0,0 +1,27 @@
+defmodule Firestorm.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      apps_path: "apps",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      aliases: aliases(),
+    ]
+  end
+
+  defp deps do
+    [
+      {:credo, "~> 0.6.1", only: [:dev, :test]},
+    ]
+  end
+
+  defp aliases do
+    [
+      "ecto.setup": ["ecto.create", "ecto.migrate", "ecto.seed"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      "test": ["ecto.drop", "ecto.create --quiet", "ecto.migrate", "test"]
+    ]
+  end
+end
