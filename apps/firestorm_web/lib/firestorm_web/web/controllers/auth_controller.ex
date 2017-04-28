c8087bc5a3a0922d9c593dd2eba7a5b7c7f2cc9c
Varun Jayaraman <varun.jayaraman@gmail.com>
2017-04-28 01:22:17 -0400

    human readable oauth error message (#66)
    * fix: prevent changeset from throwing a required field error and instead show a human readable error message for github instead of inspect changeset
    
    * chore: terminate with block with end

--- apps/firestorm_web/lib/firestorm_web/web/controllers/auth_controller.ex
+++ apps/firestorm_web/lib/firestorm_web/web/controllers/auth_controller.ex
@@ -20,24 +20,33 @@ defmodule FirestormWeb.Web.AuthController do
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
     case auth.provider do
       :github ->
-        %{name: name, nickname: nickname, email: email} = auth.info
+        with %{name: name, nickname: nickname, email: email} <- auth.info do
 
-        changeset =
-          %LoginOrRegisterFromGitHub{}
-          |> LoginOrRegisterFromGitHub.changeset(%{username: nickname, name: name, email: email})
+          changeset =
+            %LoginOrRegisterFromGitHub{}
+            |> LoginOrRegisterFromGitHub.changeset(%{username: nickname, name: name, email: email})
 
-        case LoginOrRegisterFromGitHub.run(changeset) do
-          {:ok, user} ->
-            conn
-            |> put_flash(:info, "Successfully authenticated.")
-            |> put_session(:current_user, user.id)
-            |> redirect(to: "/")
+          case LoginOrRegisterFromGitHub.run(changeset) do
+            {:ok, user} ->
+              conn
+              |> put_flash(:info, "Successfully authenticated.")
+              |> put_session(:current_user, user.id)
+              |> redirect(to: "/")
 
-          {:error, changeset} ->
-            conn
-            |> put_flash(:error, inspect changeset)
-            |> redirect(to: "/")
+            {:error, changeset} ->
+              conn
+              |> put_flash(:error, inspect changeset)
+              |> redirect(to: "/")
+          end
+        else
+          %{ email: email } -> show_github_oauth_required_field_error(conn, "username")
         end
     end
+  end
+
+  defp show_github_oauth_required_field_error(conn, field) do
+    conn
+    |> put_flash(:error, "#{field} is a required field. Please make sure your Github profile has made #{field} publicly viewable before attempting to log in again")
+    |> redirect(to: "/")
   end
 end
