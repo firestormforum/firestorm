e4a43e0d65d70480169b10b08e7975e68617440a
Josh Adams <josh.rubyist@gmail.com>
2017-04-27 22:43:32 -0500

    Implement CSS Stack and categories

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -48,6 +48,15 @@ exports.config = {
     babel: {
       // Do not use ES6 compiler in vendor code
       ignore: [/vendor/]
+    },
+    sass: {
+      options: {
+        includePaths: [
+          "node_modules/purecss/build",
+          "node_modules/normalize-css",
+          "node_modules/typeplate-starter-kit/dist/scss"
+        ]
+      }
     }
   },
 
