b0020c40ac1df24be170cd4f36b52e8d808bcfb8
Josh Adams <josh.rubyist@gmail.com>
2017-05-27 21:20:12 -0500

    Add test for adding category

--- test/feature/categories_test.exs
+++ test/feature/categories_test.exs
@@ -9,6 +9,38 @@ defmodule FirestormWeb.Feature.CategoriesTest do
     def category_title(text), do: css("h2.title", text: text)
   end
 
+  defmodule Page.CategoriesNew do
+    import Wallaby.Query
+
+    def title_field(), do: text_field("Title")
+    def create_category_button(), do: button("Create Category")
+  end
+
+  defmodule Page.Home do
+    import Wallaby.Query
+
+    def new_category_link(), do: link("New Category")
+  end
+
+  defmodule Page.CategoriesShow do
+    import Wallaby.Query
+
+    def category_title(title), do: css("h2 a", text: title)
+  end
+
+  test "creating a new category", %{session: session} do
+    import Page.CategoriesNew
+    import Page.Home
+    import Page.CategoriesShow
+
+    session
+    |> visit("/")
+    |> click(new_category_link())
+    |> fill_in(title_field(), with: "Erlang")
+    |> click(create_category_button())
+    |> assert_has(category_title("Erlang"))
+  end
+
   test "categories are listed", %{session: session} do
     import Page.CategoriesIndex
     {:ok, [_elixir, _elm]} = create_categories(["Elixir", "Elm"])
