5b13c1931edb2fe4e6ed6304080fdf609c6ecaeb
Josh Adams <josh.rubyist@gmail.com>
2017-06-01 20:50:29 -0500

    Add test for 2 posts on user show page

--- test/feature/users_test.exs
+++ test/feature/users_test.exs
@@ -3,9 +3,16 @@ defmodule FirestormWeb.Feature.UsersTest do
   alias FirestormWeb.Forums
 
   @bob %{email: "bob@example.com", name: "Bob Belcher", username: "bob"}
+  @elixir %{title: "Elixir"}
+  @otp_is_cool %{title: "OTP is cool", body: "Don't you think?"}
+  @genstage %{title: "GenStage", body: "Kinda streamy no?"}
 
   setup do
     {:ok, bob} = Forums.create_user(@bob)
+    {:ok, elixir} = Forums.create_category(@elixir)
+    {:ok, otp_is_cool} = Forums.create_thread(elixir, bob, @otp_is_cool)
+    {:ok, genstage} = Forums.create_thread(elixir, bob, @genstage)
+
     {:ok, bob: bob}
   end
 
@@ -15,7 +22,7 @@ defmodule FirestormWeb.Feature.UsersTest do
     session
     |> visit(user_path(FirestormWeb.Web.Endpoint, :show, bob))
     |> assert_has(user_details())
-    |> assert_has(user_posts())
+    |> assert_has(user_posts(2))
     |> Browser.take_screenshot()
   end
 end
