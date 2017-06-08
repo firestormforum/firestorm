6daf2ba19f8a500e5a931d681327a75b9c442a23
Josh Adams <josh.rubyist@gmail.com>
2017-06-08 01:29:12 -0500

    CSS Tweaks
    - Add page-specific classes, style each page. This is needed for
      possibly-varying padding, necessitated by user show design, but it is
      also useful if we want to provide page-specific tweaks of any other
      forms.
    - Convert various magic numbers into metric usages, color usages

--- lib/firestorm_web/web/views/layout_view.ex
+++ lib/firestorm_web/web/views/layout_view.ex
@@ -1,6 +1,26 @@
 defmodule FirestormWeb.Web.LayoutView do
   use FirestormWeb.Web, :view
 
+  def page_class(conn) do
+    [
+      "page",
+      controller_simple_name(conn),
+      Phoenix.Controller.action_name(conn)
+    ] |> Enum.join("-")
+  end
+
+  defp controller_simple_name(conn) do
+    conn
+    |> Phoenix.Controller.controller_module()
+    |> Module.split()
+    # Remove "Firestorm.Web" bit
+    |> Enum.drop(2)
+    |> hd()
+    # Remove the "Controller" bit
+    |> String.replace_trailing("Controller", "")
+    |> String.downcase()
+  end
+
   def js_script_tag do
     if Mix.env == :prod do
       "<script src=\"/js/app.js\"></script>"
