a67617fda9ebf95e14f255592907040120799b29
Josh Adams <josh.rubyist@gmail.com>
2017-06-25 12:05:31 -0500

    Add heroku configuration

--- config/prod.exs
+++ config/prod.exs
@@ -15,11 +15,19 @@ use Mix.Config
 # which you typically run after static files are built.
 config :firestorm_web, FirestormWeb.Web.Endpoint,
   on_init: {FirestormWeb.Web.Endpoint, :load_from_system_env, []},
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "firestorm-forum.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+config :firestorm_web, FirestormWeb.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # ## SSL Support
 #
@@ -57,8 +65,3 @@ config :logger, level: :info
 # start per endpoint:
 #
 #     config :firestorm_web, FirestormWeb.Web.Endpoint, server: true
-#
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
--- lib/firestorm_web/web/channels/user_socket.ex
+++ lib/firestorm_web/web/channels/user_socket.ex
@@ -6,6 +6,7 @@ defmodule FirestormWeb.Web.UserSocket do
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
