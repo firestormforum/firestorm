ffc076da607c5fdf5e059412372f55bf58bb90b3
Josh Adams <josh.rubyist@gmail.com>
2017-06-27 15:43:20 -0500

    Add favicon (#83)

--- lib/firestorm_web/web/endpoint.ex
+++ lib/firestorm_web/web/endpoint.ex
@@ -13,7 +13,7 @@ defmodule FirestormWeb.Web.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :firestorm_web, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt android-icon-192x192.png android-icon-144x144.png android-icon-96x96.png android-icon-72x72.png android-icon-48x48.png android-icon-36x36.png manifest.json favicon.ico favicon-96x96.png favicon-32x32.png favicon-16x16.png ms-icon-70x70.png ms-icon-310x310.png ms-icon-150x150.png ms-icon-144x144.png apple-icon-precomposed.png apple-icon.png apple-icon-76x76.png apple-icon-72x72.png apple-icon-60x60.png apple-icon-57x57.png apple-icon-180x180.png apple-icon-152x152.png apple-icon-144x144.png apple-icon-120x120.png apple-icon-114x114.png)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
