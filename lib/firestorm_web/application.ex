26c69794aa78f49eb342cbc5d1b4e9c131ef42cc
Josh Adams <josh.rubyist@gmail.com>
2017-07-27 03:56:21 -0500

    import supervisor spec for whole application module

--- lib/firestorm_web/application.ex
+++ lib/firestorm_web/application.ex
@@ -1,11 +1,10 @@
 defmodule FirestormWeb.Application do
   use Application
+  import Supervisor.Spec
 
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
-    import Supervisor.Spec
-
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: FirestormWeb.Supervisor]
