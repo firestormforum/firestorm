d240afbdd2c3d31f01fdb855e2ebcffb126a27ea
Josh Adams <josh.rubyist@gmail.com>
2017-08-28 21:21:38 -0400

    Add test for API to add posts

---
+++ test/web/controllers/api/v1/post_controller_test.exs
@@ -0,0 +1,13 @@
+defmodule FirestormWeb.Web.Api.V1.PostControllerTest do
+  use FirestormWeb.Web.ConnCase
+  alias FirestormWeb.Markdown
+
+  test "POST /", %{conn: conn} do
+    post = %{
+      "body" => "this is **neat**",
+    }
+    conn = post conn, "/api/v1/preview", post: post
+    response = json_response(conn, 201)["data"]
+    assert response["html"] == Markdown.render(post["body"])
+  end
+end
