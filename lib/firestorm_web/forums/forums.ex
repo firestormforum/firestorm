5acb47681d1b6e0e9b74415d48210684e30206ba
Josh Adams <josh.rubyist@gmail.com>
2017-09-01 11:33:50 -0400

    Fix crash when failing to create thread

--- lib/firestorm_web/forums/forums.ex
+++ lib/firestorm_web/forums/forums.ex
@@ -455,35 +455,39 @@ defmodule FirestormWeb.Forums do
       |> Map.take([:title])
       |> Map.put(:category_id, category.id)
 
-    {:ok, thread} =
+    thread_result =
       %{thread: thread_attrs, post: post_attrs}
       |> new_thread_changeset()
       |> Repo.insert
 
-    thread =
-      thread
-      |> Repo.preload([:category, posts: [:user]])
+    case thread_result do
+      {:ok, thread} ->
+        thread =
+          thread
+          |> Repo.preload([:category, posts: [:user]])
 
-    [thread] =
-      [thread]
-      |> decorate_threads(nil)
+        [thread] =
+          [thread]
+          |> decorate_threads(nil)
 
-    {posts, users} =
-      case thread.first_post do
-        nil -> {[], []}
-        post -> {[post], [post.user]}
-      end
+        {posts, users} =
+          case thread.first_post do
+            nil -> {[], []}
+            post -> {[post], [post.user]}
+          end
 
-    payload =
-      %ReplenishResponse{
-        categories: [thread.category],
-        threads: [thread],
-        users: users,
-        posts: posts
-      }
+        payload =
+          %ReplenishResponse{
+            categories: [thread.category],
+            threads: [thread],
+            users: users,
+            posts: posts
+          }
 
-    :ok = Endpoint.broadcast!("categories:#{thread.category_id}", "update", payload)
-    {:ok, thread}
+        :ok = Endpoint.broadcast!("categories:#{thread.category_id}", "update", payload)
+        {:ok, thread}
+      e -> e
+    end
   end
 
   defp new_thread_changeset(%{thread: thread_attrs, post: post_attrs}) do
