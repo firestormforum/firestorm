cff28a181c102933ea00274131da92c65cf2f653
Josh Adams <josh.rubyist@gmail.com>
2017-09-06 16:26:04 -0700

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule FirestormWeb.Mixfile do
 
   def project do
     [app: :firestorm_web,
-     version: "0.0.1",
+     version: "0.9.1",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
